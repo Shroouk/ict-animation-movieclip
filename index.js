8c44731129331aaa761fd18c51e3584a8cf907e2
Shroouk <shrouksaad5@gmail.com>
2024-09-16 14:31:31 +0300

    edit position

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ lib.ssMetadata = [
 
 
 
-(lib.CachedBmp_518 = function() {
+(lib.CachedBmp_566 = function() {
 	this.initialize(ss["index_atlas_2"]);
 	this.gotoAndStop(3);
 }).prototype = p = new cjs.Sprite();
@@ -443,7 +443,7 @@ lib.ssMetadata = [
 
 
 
-(lib.CachedBmp_517 = function() {
+(lib.CachedBmp_565 = function() {
 	this.initialize(ss["index_atlas_4"]);
 	this.gotoAndStop(5);
 }).prototype = p = new cjs.Sprite();
@@ -492,7 +492,7 @@ lib.ssMetadata = [
 
 
 
-(lib.CachedBmp_516 = function() {
+(lib.CachedBmp_564 = function() {
 	this.initialize(ss["index_atlas_4"]);
 	this.gotoAndStop(6);
 }).prototype = p = new cjs.Sprite();
@@ -513,7 +513,7 @@ lib.ssMetadata = [
 
 
 
-(lib.CachedBmp_515 = function() {
+(lib.CachedBmp_563 = function() {
 	this.initialize(ss["index_atlas_4"]);
 	this.gotoAndStop(7);
 }).prototype = p = new cjs.Sprite();
@@ -631,7 +631,7 @@ p.nominalBounds = new cjs.Rectangle(0,0,2671,1134);
 
 
 
-(lib.CachedBmp_514 = function() {
+(lib.CachedBmp_562 = function() {
 	this.initialize(ss["index_atlas_2"]);
 	this.gotoAndStop(9);
 }).prototype = p = new cjs.Sprite();
@@ -869,7 +869,7 @@ p.nominalBounds = new cjs.Rectangle(0,0,2671,1134);
 
 
 
-(lib.CachedBmp_513 = function() {
+(lib.CachedBmp_561 = function() {
 	this.initialize(ss["index_atlas_1"]);
 	this.gotoAndStop(49);
 }).prototype = p = new cjs.Sprite();
@@ -911,7 +911,7 @@ p.nominalBounds = new cjs.Rectangle(0,0,2671,1134);
 
 
 
-(lib.CachedBmp_512 = function() {
+(lib.CachedBmp_560 = function() {
 	this.initialize(ss["index_atlas_3"]);
 	this.gotoAndStop(12);
 }).prototype = p = new cjs.Sprite();
@@ -925,7 +925,7 @@ p.nominalBounds = new cjs.Rectangle(0,0,2671,1134);
 
 
 
-(lib.CachedBmp_511 = function() {
+(lib.CachedBmp_559 = function() {
 	this.initialize(ss["index_atlas_3"]);
 	this.gotoAndStop(13);
 }).prototype = p = new cjs.Sprite();
@@ -2657,7 +2657,7 @@ if (reversed == null) { reversed = false; }
 	cjs.MovieClip.apply(this,[props]);
 
 	// Layer_1
-	this.instance = new lib.CachedBmp_518();
+	this.instance = new lib.CachedBmp_566();
 	this.instance.setTransform(-34.2,-34.2,0.1864,0.1864);
 
 	this.instance_1 = new lib.Path_11();
@@ -3980,7 +3980,7 @@ if (reversed == null) { reversed = false; }
 	this.instance = new lib.CachedBmp_283();
 	this.instance.setTransform(13.3,11.95,0.3333,0.3333);
 
-	this.instance_1 = new lib.CachedBmp_515();
+	this.instance_1 = new lib.CachedBmp_563();
 	this.instance_1.setTransform(-0.45,-0.45,0.3333,0.3333);
 
 	this.instance_2 = new lib.CachedBmp_281();
@@ -4001,7 +4001,7 @@ if (reversed == null) { reversed = false; }
 	this.instance_7 = new lib.CachedBmp_286();
 	this.instance_7.setTransform(13.3,4.4,0.3333,0.3333);
 
-	this.instance_8 = new lib.CachedBmp_516();
+	this.instance_8 = new lib.CachedBmp_564();
 	this.instance_8.setTransform(-0.45,-0.45,0.3333,0.3333);
 
 	this.instance_9 = new lib.CachedBmp_284();
@@ -4022,7 +4022,7 @@ if (reversed == null) { reversed = false; }
 	this.instance_14 = new lib.CachedBmp_293();
 	this.instance_14.setTransform(13.3,4.4,0.3333,0.3333);
 
-	this.instance_15 = new lib.CachedBmp_517();
+	this.instance_15 = new lib.CachedBmp_565();
 	this.instance_15.setTransform(-0.45,-0.45,0.3333,0.3333);
 
 	this.instance_16 = new lib.CachedBmp_291();
@@ -4134,7 +4134,7 @@ if (reversed == null) { reversed = false; }
 	this.instance = new lib.CachedBmp_264();
 	this.instance.setTransform(13.85,0,0.3333,0.3333);
 
-	this.instance_1 = new lib.CachedBmp_514();
+	this.instance_1 = new lib.CachedBmp_562();
 	this.instance_1.setTransform(0,0.55,0.3333,0.3333);
 
 	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.instance}]}).wait(1));
@@ -4516,7 +4516,7 @@ if (reversed == null) { reversed = false; }
 	cjs.MovieClip.apply(this,[props]);
 
 	// Layer_1
-	this.instance = new lib.CachedBmp_513();
+	this.instance = new lib.CachedBmp_561();
 	this.instance.setTransform(2.15,6.2,0.3333,0.3333);
 
 	this.instance_1 = new lib.CachedBmp_227();
@@ -4574,13 +4574,13 @@ if (reversed == null) { reversed = false; }
 	this.instance = new lib.CachedBmp_221();
 	this.instance.setTransform(0.85,-17.9,0.3333,0.3333);
 
-	this.instance_1 = new lib.CachedBmp_511();
+	this.instance_1 = new lib.CachedBmp_559();
 	this.instance_1.setTransform(-81.15,-15,0.3333,0.3333);
 
 	this.instance_2 = new lib.CachedBmp_223();
 	this.instance_2.setTransform(0.85,-17.85,0.3333,0.3333);
 
-	this.instance_3 = new lib.CachedBmp_512();
+	this.instance_3 = new lib.CachedBmp_560();
 	this.instance_3.setTransform(-81.15,-14.95,0.3333,0.3333);
 
 	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.instance}]}).to({state:[{t:this.instance_3},{t:this.instance_2}]},1).wait(2));
@@ -36092,7 +36092,7 @@ if (reversed == null) { reversed = false; }
 
 	this.button5 = new lib.ClipGroup_5();
 	this.button5.name = "button5";
-	this.button5.setTransform(-308.4,-59.4,1,1,0,0,0,73.6,12.2);
+	this.button5.setTransform(-309.4,-58.4,1,1,0,0,0,73.6,12.2);
 
 	this.button6 = new lib.ClipGroup_4();
 	this.button6.name = "button6";
@@ -38890,43 +38890,43 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/CachedBmp_267.png?1726482100680", id:"CachedBmp_267"},
-		{src:"images/index_atlas_1.png?1726482098245", id:"index_atlas_1"},
-		{src:"images/index_atlas_2.png?1726482098246", id:"index_atlas_2"},
-		{src:"images/index_atlas_3.png?1726482098247", id:"index_atlas_3"},
-		{src:"images/index_atlas_4.png?1726482098248", id:"index_atlas_4"},
-		{src:"images/index_atlas_5.png?1726482098249", id:"index_atlas_5"},
-		{src:"images/index_atlas_6.png?1726482098249", id:"index_atlas_6"},
-		{src:"images/index_atlas_7.png?1726482098250", id:"index_atlas_7"},
-		{src:"images/index_atlas_8.png?1726482098315", id:"index_atlas_8"},
-		{src:"images/index_atlas_9.png?1726482098316", id:"index_atlas_9"},
-		{src:"images/index_atlas_10.png?1726482098317", id:"index_atlas_10"},
-		{src:"images/index_atlas_11.png?1726482098317", id:"index_atlas_11"},
-		{src:"images/index_atlas_12.png?1726482098318", id:"index_atlas_12"},
-		{src:"images/index_atlas_13.png?1726482098319", id:"index_atlas_13"},
-		{src:"images/index_atlas_14.png?1726482098320", id:"index_atlas_14"},
-		{src:"images/index_atlas_15.png?1726482098321", id:"index_atlas_15"},
-		{src:"images/index_atlas_16.png?1726482098321", id:"index_atlas_16"},
-		{src:"images/index_atlas_17.png?1726482098322", id:"index_atlas_17"},
-		{src:"images/index_atlas_18.png?1726482098323", id:"index_atlas_18"},
-		{src:"images/index_atlas_19.png?1726482098324", id:"index_atlas_19"},
-		{src:"images/index_atlas_20.png?1726482098325", id:"index_atlas_20"},
-		{src:"images/index_atlas_21.png?1726482098326", id:"index_atlas_21"},
-		{src:"images/index_atlas_22.png?1726482098326", id:"index_atlas_22"},
-		{src:"images/index_atlas_23.png?1726482098327", id:"index_atlas_23"},
-		{src:"images/index_atlas_24.png?1726482098328", id:"index_atlas_24"},
-		{src:"images/index_atlas_25.png?1726482098329", id:"index_atlas_25"},
-		{src:"images/index_atlas_26.png?1726482098330", id:"index_atlas_26"},
-		{src:"images/index_atlas_27.png?1726482098331", id:"index_atlas_27"},
-		{src:"images/index_atlas_28.png?1726482098331", id:"index_atlas_28"},
-		{src:"images/index_atlas_29.png?1726482098332", id:"index_atlas_29"},
-		{src:"images/index_atlas_30.png?1726482098333", id:"index_atlas_30"},
-		{src:"images/index_atlas_31.png?1726482098333", id:"index_atlas_31"},
-		{src:"images/index_atlas_32.png?1726482098334", id:"index_atlas_32"},
-		{src:"https://code.jquery.com/jquery-3.4.1.min.js?1726482100680", id:"lib/jquery-3.4.1.min.js"},
-		{src:"components/sdk/anwidget.js?1726482100680", id:"sdk/anwidget.js"},
-		{src:"components/ui/src/textinput.js?1726482100680", id:"an.TextInput"},
-		{src:"components/ui/src/css.js?1726482100680", id:"an.CSS"}
+		{src:"images/CachedBmp_267.png?1726486267045", id:"CachedBmp_267"},
+		{src:"images/index_atlas_1.png?1726486265432", id:"index_atlas_1"},
+		{src:"images/index_atlas_2.png?1726486265432", id:"index_atlas_2"},
+		{src:"images/index_atlas_3.png?1726486265433", id:"index_atlas_3"},
+		{src:"images/index_atlas_4.png?1726486265433", id:"index_atlas_4"},
+		{src:"images/index_atlas_5.png?1726486265433", id:"index_atlas_5"},
+		{src:"images/index_atlas_6.png?1726486265434", id:"index_atlas_6"},
+		{src:"images/index_atlas_7.png?1726486265434", id:"index_atlas_7"},
+		{src:"images/index_atlas_8.png?1726486265434", id:"index_atlas_8"},
+		{src:"images/index_atlas_9.png?1726486265434", id:"index_atlas_9"},
+		{src:"images/index_atlas_10.png?1726486265434", id:"index_atlas_10"},
+		{src:"images/index_atlas_11.png?1726486265435", id:"index_atlas_11"},
+		{src:"images/index_atlas_12.png?1726486265435", id:"index_atlas_12"},
+		{src:"images/index_atlas_13.png?1726486265435", id:"index_atlas_13"},
+		{src:"images/index_atlas_14.png?1726486265435", id:"index_atlas_14"},
+		{src:"images/index_atlas_15.png?1726486265435", id:"index_atlas_15"},
+		{src:"images/index_atlas_16.png?1726486265435", id:"index_atlas_16"},
+		{src:"images/index_atlas_17.png?1726486265435", id:"index_atlas_17"},
+		{src:"images/index_atlas_18.png?1726486265436", id:"index_atlas_18"},
+		{src:"images/index_atlas_19.png?1726486265436", id:"index_atlas_19"},
+		{src:"images/index_atlas_20.png?1726486265436", id:"index_atlas_20"},
+		{src:"images/index_atlas_21.png?1726486265436", id:"index_atlas_21"},
+		{src:"images/index_atlas_22.png?1726486265436", id:"index_atlas_22"},
+		{src:"images/index_atlas_23.png?1726486265437", id:"index_atlas_23"},
+		{src:"images/index_atlas_24.png?1726486265437", id:"index_atlas_24"},
+		{src:"images/index_atlas_25.png?1726486265437", id:"index_atlas_25"},
+		{src:"images/index_atlas_26.png?1726486265437", id:"index_atlas_26"},
+		{src:"images/index_atlas_27.png?1726486265437", id:"index_atlas_27"},
+		{src:"images/index_atlas_28.png?1726486265438", id:"index_atlas_28"},
+		{src:"images/index_atlas_29.png?1726486265438", id:"index_atlas_29"},
+		{src:"images/index_atlas_30.png?1726486265438", id:"index_atlas_30"},
+		{src:"images/index_atlas_31.png?1726486265438", id:"index_atlas_31"},
+		{src:"images/index_atlas_32.png?1726486265438", id:"index_atlas_32"},
+		{src:"https://code.jquery.com/jquery-3.4.1.min.js?1726486267045", id:"lib/jquery-3.4.1.min.js"},
+		{src:"components/sdk/anwidget.js?1726486267045", id:"sdk/anwidget.js"},
+		{src:"components/ui/src/textinput.js?1726486267045", id:"an.TextInput"},
+		{src:"components/ui/src/css.js?1726486267045", id:"an.CSS"}
 	],
 	preloads: []
 };
